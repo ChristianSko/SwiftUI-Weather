299a0a81b106e0c6aa0ac5c95e9486d572224632
Christian Skorobogatow <christiansko@icloud.com>
2020-12-28 16:18:20 +0100

    Weather App, first version

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -8,14 +8,151 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isNight = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            BackgroundView(isNight: $isNight)
+            VStack(spacing: 30){
+                CityTextView(cityName: "Cupertino, CA")
+                
+                todayWeatherView(imageName: isNight ? "moon.stars.fill" :  "cloud.sun.fill",
+                                 temperature: 76)
+                
+                Spacer()
+                
+                WeekView()
+                
+                Spacer()
+                
+                Button {
+                    isNight.toggle()
+                } label: {
+                    WeatherButtonView(title: "Change Day Time",
+                                      textColor: .blue,
+                                      backgroundColor: .white)
+                }
+                
+                Spacer()
+            }
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+struct WeatherDayview: View {
+    
+    var dayOftheWeek: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack(spacing: 10){
+            Text(dayOftheWeek)
+                .font(.system(size: 16, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            
+            Text("\(temperature)º")
+                .font(.system(size: 28, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            
+        }
+    }
+}
+
+struct BackgroundView: View {
+    
+    @Binding var isNight: Bool
+    
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [isNight ? .black : .blue,
+                                                   isNight ? .gray : Color("lighBlue")]),
+                       startPoint: .topLeading,
+                       endPoint: .bottomTrailing)
+            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CityTextView: View {
+    var cityName: String
+    
+    var body: some View{
+        
+        Text(cityName)
+            .font(.system(size: 32, weight: .medium, design: .default))
+            .foregroundColor(.white)
+            .padding(.top)
+    }
+}
+
+
+struct todayWeatherView: View {
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View{
+        VStack(spacing: 10){
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 180, height: 180)
+            
+            Text("\(temperature)º")
+                .font(.system(size: 70, weight: .medium))
+                .foregroundColor(.white)
+        }
+    }
+}
+
+struct WeatherButtonView: View {
+    
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+        
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundColor)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight: .bold, design: .default))
+            .cornerRadius(10)
+        
+    }
+}
+
+struct WeekView: View {
+    
+    let weekdays = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
+    var weatherImages = ["cloud.sun.fill","cloud.sun.fill","cloud.sun.fill", "sun.max.fill","wind.snow","sunset.fill","snow"]
+    var degrees = [20, 22, 74, 88, 55, 60, 25]
+    
+    var body: some View {
+        HStack(spacing: 20){
+            ForEach(2..<weekdays.count) { day in
+                
+                WeatherDayview(dayOftheWeek: weekdays[day],
+                               imageName: weatherImages[day],
+                               temperature: degrees[day])
+                
+            }
+            
+        }
     }
 }
